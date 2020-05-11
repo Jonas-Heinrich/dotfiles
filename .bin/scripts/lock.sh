f116e37f9072e6a5408658fa035a58a0278ef0c5
Jonas Heinrich <Jonas@JonasHeinrich.com>
2020-05-11 12:05:12 +0200

    Rice i3
    - Adapt arc-dark theme with `gtk`, `i3`
    - Include custom lock script
    - Add `compton` for transparent windows
    - Add `i3blocks` for status bar

---
+++ .bin/scripts/lock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+
+# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+# i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
+i3lock -e -u -n -i /tmp/screen.png
+
+rm /tmp/screen.png
